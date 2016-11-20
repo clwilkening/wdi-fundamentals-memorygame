34c7fa0b8090cc70b6057c77db78590206e61ab2
Christopher Wilkening <clw13clw13@gmail.com>
2016-11-20 10:12:06 -0600

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,8 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo){
+		alert("You found a match!");
+}	else
+		alert("Sorry, try again");
