1f94a06d29541daf0c4c11e66103c321c8898f28
Christopher Wilkening <clw13clw13@gmail.com>
2016-11-21 13:49:45 -0500

    added JavaScript to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,7 +5,17 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo){
-		alert("You found a match!");
-}	else
-		alert("Sorry, try again");
+//if (cardOne === cardTwo){
+//		alert("You found a match!");
+//}	else
+//		alert("Sorry, try again");
+
+var board = document.getElementById('game-board');
+function createCards() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+		 }
+	createCards(4);
+}
